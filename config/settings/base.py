83a78ca7ea46ba7ec13e8f96560fbcee60fab59b
Ryan <sailantht@gmail.com>
2023-08-30 18:36:14 +0330

    Initialize dockerization

--- config/settings/base.py
+++ config/settings/base.py
@@ -130,8 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "staticfiles/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"
 # ]
