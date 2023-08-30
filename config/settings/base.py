9ae9c5202eaffb1ee9827370be36b4cb26e9dfdc
Ryan <sailantht@gmail.com>
2023-08-30 18:40:46 +0330

    Revert "Initialize dockerization"
    This reverts commit 83a78ca7ea46ba7ec13e8f96560fbcee60fab59b.

--- config/settings/base.py
+++ config/settings/base.py
@@ -130,11 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = "staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"
 # ]
