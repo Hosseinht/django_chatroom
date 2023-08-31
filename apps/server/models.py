8dd86c2bced3316dbfc5c7efde1d8de68a9afa7b
Ryan <sailantht@gmail.com>
2023-08-31 19:02:24 +0330

    Create category model

--- apps/server/models.py
+++ apps/server/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
