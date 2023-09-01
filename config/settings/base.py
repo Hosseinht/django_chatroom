74527ca469cbd557c71d419b52df4e0f90d58903
Ryan <sailantht@gmail.com>
2023-09-01 11:34:55 +0330

    Configure basic authentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -149,6 +149,9 @@ AUTH_USER_MODEL = "user.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
