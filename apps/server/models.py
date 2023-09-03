09511623d546758ca9cecf1c7e85e311a009a52b
Ryan <sailantht@gmail.com>
2023-09-03 10:22:07 +0330

    Handle upload, update and delete category icon

--- apps/server/models.py
+++ apps/server/models.py
@@ -1,5 +1,9 @@
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
+
+from .utils import category_icon_upload_path
 
 User = settings.AUTH_USER_MODEL
 
@@ -7,6 +11,25 @@ User = settings.AUTH_USER_MODEL
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(upload_to=category_icon_upload_path, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        """
+        Custom save method for updating and managing 'icon' files.
+        """
+        if self.id:
+            current_category = get_object_or_404(Category, id=self.id)
+            if current_category.icon != self.icon:
+                current_category.icon.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
---
+++ apps/server/utils.py
@@ -0,0 +1,2 @@
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.name}/category_icon/{filename}"
