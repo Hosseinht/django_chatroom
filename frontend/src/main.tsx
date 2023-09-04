fb6d89262111b61a6f0d75cf19d1a121d3dd9869
Ryan <sailantht@gmail.com>
2023-09-04 16:18:47 +0330

    Install Chakra UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router";
+
+import router from "./router.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>,
-)
+);
---
+++ frontend/src/router.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+
+export default router;
