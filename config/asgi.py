ed4720ef7785f3100b8ae15b87da5f38d2080ba1
Ryan <sailantht@gmail.com>
2023-09-09 15:44:32 +0330

    Configure websockets routing

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,21 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 django_application = get_asgi_application()
+
+from . import urls  # noqa isort:skip
+
+# Setting up router
+application = ProtocolTypeRouter(
+    # separate and redirect different types of protocol
+    {
+        "http": get_asgi_application(),
+        # "websocket": URLRouter(
+        #     urls.websocket_urlpatterns,
+        # ),
+    }
+)
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView,
     SpectacularRedocView,
+    SpectacularSwaggerView,
 )
 
 urlpatterns = [
@@ -25,5 +25,8 @@ urlpatterns = [
         name="redoc",
     ),
 ]
+
+# websocket_urlpatterns = [path()]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
